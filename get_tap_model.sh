7858637c7b365bc25ef11cb786d25b7fd5b4984a
namanambu <143414186+namanambu@users.noreply.github.com>
2025-02-15 21:47:04 -0500

    changed wget to curl
    works better for gitbash

--- get_tap_model.sh
+++ get_tap_model.sh
@@ -1,8 +1,8 @@
 mkdir -p ./trained_models/cotracker
 cd ./trained_models/cotracker
 
-wget https://dl.fbaipublicfiles.com/cotracker/cotracker_stride_4_wind_8.pth
-wget https://dl.fbaipublicfiles.com/cotracker/cotracker_stride_4_wind_12.pth
-wget https://dl.fbaipublicfiles.com/cotracker/cotracker_stride_8_wind_16.pth
+curl -O https://dl.fbaipublicfiles.com/cotracker/cotracker_stride_4_wind_8.pth
+curl -O https://dl.fbaipublicfiles.com/cotracker/cotracker_stride_4_wind_12.pth
+curl -O https://dl.fbaipublicfiles.com/cotracker/cotracker_stride_8_wind_16.pth
 
 cd ../..
