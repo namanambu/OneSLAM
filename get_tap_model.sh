702ce35e644bf77cde9a62b4894382c9ca21b93b
namanambu <143414186+namanambu@users.noreply.github.com>
2025-02-23 12:15:06 -0500

    revert back to wget

--- get_tap_model.sh
+++ get_tap_model.sh
@@ -1,8 +1,8 @@
 mkdir -p ./trained_models/cotracker
 cd ./trained_models/cotracker
 
-curl -O https://dl.fbaipublicfiles.com/cotracker/cotracker_stride_4_wind_8.pth
-curl -O https://dl.fbaipublicfiles.com/cotracker/cotracker_stride_4_wind_12.pth
-curl -O https://dl.fbaipublicfiles.com/cotracker/cotracker_stride_8_wind_16.pth
+wget https://dl.fbaipublicfiles.com/cotracker/cotracker_stride_4_wind_8.pth
+wget https://dl.fbaipublicfiles.com/cotracker/cotracker_stride_4_wind_12.pth
+wget https://dl.fbaipublicfiles.com/cotracker/cotracker_stride_8_wind_16.pth
 
 cd ../..
